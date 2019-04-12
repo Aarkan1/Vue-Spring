67f50c846b6826304a3213fbe9e17d74bc03a482
Aarkan <johanwiren1@gmail.com>
2019-04-12 19:07:07 +0200

    cleanup

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   devServer: {
     proxy: {
-      "/api/posts": {
-        target: "http://localhost:8080",
-        ws: true,
-        changeOrigin: true,
-        secure: false
-      },
-      "/api/posts/:id": {
-        target: "http://localhost:8080",
-        ws: true,
-        changeOrigin: true,
-        secure: false
-      },
-      "/api/users": {
-        target: "http://localhost:8080",
-        ws: true,
-        changeOrigin: true,
-        secure: false
-      },
-      "/api/users/:id": {
+      "/api": {
         target: "http://localhost:8080",
         ws: true,
         changeOrigin: true,
--- src/main/java/web/controllers/MyWebMvcConfigurer.java
+++ src/main/java/web/controllers/MyWebMvcConfigurer.java
@@ -1,15 +1,16 @@
 package web.controllers;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class MyWebMvcConfigurer implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("*.*").addResourceLocations(
-                "file:src/main/resources/static/"
-        );
-    }
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**")
+            .addResourceLocations("file:src/main/resources/static/")
+            .setCacheControl(CacheControl.noCache());
+  }
 }
